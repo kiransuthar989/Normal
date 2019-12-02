3c4b22a669bea97c941231e6890ff29f6ab134df
kiran suthar <kiransuthar789outlook.com>
2019-12-02 21:29:53 +0530

    software1.java

--- software1.java
+++ software1.java
@@ -3,6 +3,10 @@ class T
 {
 public static void main (String []args)
 {
-
+System.out.println("Enter the number = ");
+Scanner x = new scanner(System.in);
+int a = x.nextInt();
+int c = (a - 1)%2;
+System.out.println("Output is = "+c + 2);
 }
 }
